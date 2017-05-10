6709b82cee417ed700b198c4bce89619977fe10b
fmcarvalho <mcarvalho@cc.isel.ipl.pt>
2017-05-10 18:47:49 +0100

    Averager

--- aula23-reduction/src/main/java/App.java
+++ aula23-reduction/src/main/java/App.java
@@ -15,22 +15,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import util.StreamUtils;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Spliterator;
-import java.util.Spliterators.AbstractSpliterator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static java.lang.System.out;
 import static java.util.Arrays.stream;
@@ -42,24 +33,97 @@ public class App {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
         Path p = Paths.get(ClassLoader.getSystemResource(SRC).toURI());
-        String[] lines = Files.lines(p).toArray(String[]::new);
+        Stream<String> lines = Files.lines(p);
+        int[] temperatures = parseTemperature(lines).toArray();
+        // out.println(streamMax(lines));
+        out.println(intStreamMax(stream(temperatures)));
+        for (int i = 0; i < 10; i++) {
+            // out.println(streamAverage1(stream(temperatures)));
+            out.println(streamAverage2(stream(temperatures)));
+            // out.println(streamAverage3(stream(temperatures)));
+        }
+    }
 
-        filterEvenLine( stream(lines)
+    /**
+     * !!!! Side-effects => mutable shared state
+     * !!!! Auxiliary int array od temperatures int[] temps
+     * !!!! 2x iterate sequence!!!
+     */
+    static double streamAverage1(IntStream temperatures) {
+        int[] temps = temperatures
+                .parallel()
+                .toArray();
+        int[] sum = {0};
+        stream(temps).parallel().forEach(t -> sum[0] += t);
+        return ((double)sum[0])/temps.length;
+    }
+
+    /**
+     * !!!! Side-effects => mutable shared state: sum and count
+     */
+    static double streamAverage2(IntStream temperatures) {
+        int[] sum = {0};
+        int[] count = {0};
+        temperatures
+                .parallel()
+                .forEach(t -> {
+                    sum[0] += t;
+                    count[0]++;
+                });
+        return ((double)sum[0])/count[0];
+    }
+
+    static double streamAverage3(IntStream temperatures) {
+        Averager avg = temperatures
+                .mapToObj(nr -> new Averager(nr))
+                .parallel()
+                .reduce((prev, curr) -> prev.add(curr))
+                .get();
+        return ((double)avg.sum)/avg.count;
+    }
+
+    static class Averager{
+        final int sum;
+        final int count;
+
+        public Averager(int sum, int count) {
+            this.sum = sum;
+            this.count = count;
+        }
+
+        public Averager(int nr) {
+            sum = nr; count = 1;
+        }
+
+        public Averager add(Averager other) {
+            return new Averager(this.sum + other.sum, this.count + other.count);
+        }
+    }
+
+    static int streamMax(Stream<String> lines) {
+        return filterEvenLine(lines
                 .filter(s -> !s.startsWith("#"))// Filter comments
                 .skip(1)                        // Skip line: Not Available
             )                                   // Filter even lines
-            .map(l -> l.substring(14, 16))      // Extract Temperature
-            .map(Integer::parseInt)             // Stream<Integer>
-            .max(Integer::compare)              // Optional<Integer>
-            .ifPresent(out::println);
+                .map(l -> l.substring(14, 16))  // Extract Temperature
+                .map(Integer::parseInt)         // -> Boxing
+                .max(Integer::compare)          // -> Unboxing
+                .get();
+    }
 
-        filterEvenLine( stream(lines)
-                .filter(s -> !s.startsWith("#"))// Filter comments
-                .skip(1)                        // Skip line: Not Available
-            )                                   // Filter even lines
-            .map(l -> l.substring(14, 16))      // Extract Temperature
-            .mapToInt(Integer::parseInt)        // IntStream
+
+    static int intStreamMax(IntStream temperatures) {
+        return temperatures
             .max()                              // OptionalInt
-            .ifPresent(out::println);
+            .getAsInt();
+    }
+
+    static IntStream parseTemperature(Stream<String> lines) {
+        return filterEvenLine(lines
+                .filter(s -> !s.startsWith("#"))// Filter comments
+                .skip(1)                        // Skip line: Not Available
+            )                                   // Filter even lines
+            .map(l -> l.substring(14, 16))      // Extract Temperature
+            .mapToInt(Integer::parseInt);
     }
 }
